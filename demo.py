2fddc61cfc29a93fea8843f957422ed7b1915dac
kiran umarani <kiranumarni200@gmail.com>
2026-07-02 08:48:05 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("name: ",self.name)
+        print("age: ",self.age)
+
+s1 = student("kiran",23)
+
+s1.display()        
